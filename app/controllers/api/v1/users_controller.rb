181c49bbf1500867ef60b6cd3b60432ac3a6aea6
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-20 21:26:53 +0300

    modify the user register method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,14 @@ class Api::V1::UsersController < ApplicationController
     def create 
         @user = User.create(user_params)
         if @user.valid?
-            token = encode_token({user_id: @user.id})
-            render json: {user: @user, token: token}, status: :created
+            @token = encode_token({user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
         else
             render json: {error: 'Invalid username or password'}, status: :unprocessable_entity
         end
     end
+
+    def 
 
     private
     def user_params
